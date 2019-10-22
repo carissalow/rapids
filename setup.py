41c233e4eda87c053297cc324b4eb313498e4668
JulioV <JulioV@users.noreply.github.com>
2019-10-22 13:11:01 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data cleaning, feature engineering and analysis for Aware sensors',
+    author='Moshi',
+    license='MIT',
+)
