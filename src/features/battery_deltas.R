30f814887dd012ae45a1dc7a03adbce9d56d93bb
JulioV <JulioV@users.noreply.github.com>
2019-11-21 11:34:30 -0500

    Add local_day_segment to battery deltas

--- src/features/battery_deltas.R
+++ src/features/battery_deltas.R
@@ -15,7 +15,8 @@ if(nrow(battery) > 0){
               local_start_date_time = first(local_date_time),
               local_end_date_time = last(local_date_time),
               local_start_date = first(local_date),
-              local_end_date = last(local_date)) %>%
+              local_end_date = last(local_date),
+              local_day_segment = first(local_day_segment)) %>%
     select(-group_id) %>%
     filter(time_diff > 0.1) # Avoids including quick cycles
 } else {
@@ -24,7 +25,8 @@ if(nrow(battery) > 0){
                             local_start_date_time = character(),
                             local_end_date_time = character(),
                             local_start_date = character(),
-                            local_end_date = character())
+                            local_end_date = character(),
+                            local_day_segment = character())
 }
 
 write.csv(consumption, snakemake@output[[1]], row.names = FALSE)
