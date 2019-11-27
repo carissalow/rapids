e5390ee0e2a4b5ffaaae8b96c90bb236a8a70577
JulioV <JulioV@users.noreply.github.com>
2019-11-27 16:06:30 -0500

    Fix column name for gar deltas

--- src/features/google_activity_recognition_deltas.R
+++ src/features/google_activity_recognition_deltas.R
@@ -10,7 +10,7 @@ if(nrow(gar) > 0){
     mutate(activity_episode = cumsum(c(1, head(activity_type, -1) != tail(activity_type, -1)))) %>% 
     group_by(activity_episode) %>%
     filter(n() > 1) %>%
-    summarize(episode = first(activity_name),
+    summarize(activity = first(activity_name),
               time_diff = (last(timestamp) - first(timestamp)) / (1000 * 60),
               local_start_date_time = first(local_date_time),
               local_end_date_time = last(local_date_time),
@@ -20,7 +20,7 @@ if(nrow(gar) > 0){
               local_end_day_segment = last(local_day_segment)) %>%
     select(-activity_episode)
 } else {
-  activity_episodes <- data.frame(episode = character(), 
+  activity_episodes <- data.frame(activity = character(), 
                             time_diff = numeric(),
                             local_start_date_time = character(),
                             local_end_date_time = character(),
