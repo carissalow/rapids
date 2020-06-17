5627a73a678f80be95fe39b4c5ad67c385d56282
kaguillera <kaguillera@users.noreply.github.com>
2020-06-17 16:23:00 -0400

    Added testing for screen features (android)

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # Commmands necessary to setup and run the tests for RAPIDS
 
+echo Setting up for testing...
+
+echo Copying files...
 cp -r tests/data/raw/* data/raw
-cp tests/data/external/* data/external 
+cp tests/data/external/* data/external
+
+echo Disabling downloading of dataset...
 sed -e '10,20 s/^/#/' -e  's/rules.download_dataset.output/"data\/raw\/\{pid\}\/\{sensor\}_raw\.csv"/' rules/preprocessing.snakefile > tmp
 cp tmp rules/preprocessing.snakefile
+
+echo Disabling downloading of dataset...
 snakemake --profile tests/settings
+
+echo Running tests on data produced...
 python -m unittest discover tests/scripts/ -v 
