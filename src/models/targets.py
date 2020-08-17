00385dc54dd56e367d173f9937c119b4338a2578
Meng Li <34143965+Meng6@users.noreply.github.com>
2020-08-17 10:48:15 -0400

    Discard useless parameters and related code of example

--- src/models/targets.py
+++ src/models/targets.py
@@ -3,19 +3,10 @@ import numpy as np
 
 pid = snakemake.params["pid"]
 summarised = snakemake.params["summarised"]
-targets_ratio_threshold = snakemake.params["targets_ratio_threshold"]
-targets_value_threshold = snakemake.params["targets_value_threshold"]
 participant_info = pd.read_csv(snakemake.input["participant_info"])
 
 if summarised == "summarised":
-    targets = pd.DataFrame(columns=["pid", "target"])
-
-    if not participant_info.empty:
-        cesds = participant_info.loc[0, ["preop_cesd_total", "inpatient_cesd_total", "postop_cesd_total", "3month_cesd_total"]]
-        # targets: 1 => 50% (ceiling) or more of available CESD scores were 16 or higher; 0 => otherwise
-        num_threshold = int((cesds.count() + 1) * targets_ratio_threshold)
-        target = 1 if cesds.apply(lambda x : 1 if x >= targets_value_threshold else 0).sum() >= num_threshold else 0
-        targets.loc[0, :] = [pid, target]
+    raise ValueError("Do not support summarised features for example dataset.")
 
 elif summarised == "notsummarised":
     targets = participant_info[["local_date", "target"]]
