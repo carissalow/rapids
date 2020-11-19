a10c8e24aad356a4f5b044bbac4893453b998e29
JulioV <JulioV@users.noreply.github.com>
2020-11-18 19:14:16 -0500

    Fix banner for old versions of the docs

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,18 +1,5 @@
 window.addEventListener("DOMContentLoaded", function() {
-  
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", window.location + "../versions.json");
-    xhr.onload = function() {
-      var versions = JSON.parse(this.responseText);
-  
-      var realVersion = versions.find(function(i) {
-        return i.version === CURRENT_VERSION ||
-               i.aliases.includes(CURRENT_VERSION);
-      }).version;
-
-      console.log(version)
-      console.log(CURRENT_VERSION)
-    };
-    xhr.send();
+    if(!window.location.pathname.endsWith("/latest/")){
+        document.querySelector("div[data-md-component=announce]").innerHTML = "You are seeing the docs for a previous version of RAPIDS, <a href='latest/'>click here to go to the latest</a>"
+    }
   });
-  
