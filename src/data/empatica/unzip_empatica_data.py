5f5f19866f8cebc4efaf7247866a3ebfa1b817ec
Juseong Kim <juseong.kim@hotmail.com>
2020-12-30 23:17:30 +0900

    implement extract_empatica_data script
    add support for all data types
    
    Fix name comparison of zipped files

--- src/data/empatica/unzip_empatica_data.py
+++ src/data/empatica/unzip_empatica_data.py
@@ -1,19 +1,19 @@
 from zipfile import ZipFile
 import warnings
-sensor_short_name = {"accelerometer":"acc",
-                "temperature":"temp",
+sensor_short_name = {"accelerometer":"ACC",
+                "temperature":"TEMP",
                 "tags":"tags",
-                "heartrate":"hr",
-                "inter_beat_interval":"ibi",
-                "blood_volume_pulse":"bvp",
-                "electrodermal_activity":"eda"}
+                "heartrate":"HR",
+                "inter_beat_interval":"IBI",
+                "blood_volume_pulse":"BVP",
+                "electrodermal_activity":"EDA"}
 
 sensor_csv = sensor_short_name[snakemake.params["sensor"]] + '.csv'
 warning = True
 with ZipFile(snakemake.input[0], 'r') as zipFile:
     listOfFileNames = zipFile.namelist()
     for fileName in listOfFileNames:
-        if fileName.endswith(sensor_csv):
+        if fileName == sensor_csv:
             with open(snakemake.output[0], 'wb') as outputFile:
                 outputFile.write(zipFile.read(fileName))
                 warning = False
