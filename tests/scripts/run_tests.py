aafe35107da028350d62b12bb002dacb44bd2676
JulioV <JulioV@users.noreply.github.com>
2021-04-08 16:14:10 -0400

    Fix missing pid files directory

--- tests/scripts/run_tests.py
+++ tests/scripts/run_tests.py
@@ -37,7 +37,8 @@ class RapidsTests(unittest.TestCase):
         # Loop through the file_list dictionary and check if the files exist. 
         file_lists = self.generate_sensor_file_lists()
         for each in file_lists:
-            print("The actual output file should exist: {}".format(each[0]))
+            print("Checking if the output file exists: {}".format(each[0]))
+            print(os.path.exists( each[0]))
             self.assertEqual(os.path.exists( each[0]), 1)
 
 
@@ -48,12 +49,12 @@ class RapidsTests(unittest.TestCase):
             df_exp = pd.read_csv(exp_result)
             if df_act.empty:
                 print(act_result)
-                print("The expected output should be empty: {}".format(exp_result))
+                print("Checking if the output file is empty: {}".format(exp_result))
                 self.assertTrue(df_exp.empty)
             else:
                 # The order in which the columns varies from time to time so
                 # the columns are sorted before doing the comparision 
-                print("Comparing: {} and {}".format(act_result, exp_result))
+                print("Comparing {} and {}".format(act_result, exp_result))
                 df_exp = df_exp.reindex(sorted(df_exp.columns), axis=1)
                 df_act = df_act.reindex(sorted(df_act.columns), axis=1)
                 pd.testing.assert_frame_equal(df_exp, df_act, obj=df_exp)
@@ -94,7 +95,7 @@ def run_some_tests(test_type):
     elif test_type == "event":
         test_class = TestEvent
     else:
-        raise ValueError("Only frequency or periodic are valid arguments")
+        raise ValueError("Only frequency, periodic, and event are valid arguments")
     loader = unittest.TestLoader()
 
     suite = loader.loadTestsFromTestCase(test_class)
