f0ac610083ca51cf065fa16e47701574dfba6b35
Jennifer Fedor <fedorjm@gmail.com>
2023-05-15 18:00:32 -0400

    Add new Fitbit activity fragmentation and walking cadence features

--- docs/features/fitbit-steps-intraday.md
+++ docs/features/fitbit-steps-intraday.md
@@ -27,38 +27,67 @@ Parameters description for `[FITBIT_STEPS_INTRADAY][PROVIDERS][RAPIDS]`:
 
 |Key&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;            | Description |
 |----------------|-----------------------------------------------------------------------------------------------------------------------------------
-|`[COMPUTE]`                | Set to `True` to extract `FITBIT_STEPS_INTRADAY` features from the `RAPIDS` provider|
-|`[FEATURES]`               |         Features to be computed from steps intraday data, see table below           |
-|`[REFERENCE_HOUR]`         | The reference point from which `firststeptime` or `laststeptime` is to be computed, default is midnight |
-|`[THRESHOLD_ACTIVE_BOUT]`  | Every minute with Fitbit steps data wil be labelled as `sedentary` if its step count is below this threshold, otherwise, `active`.    |
-|`[INCLUDE_ZERO_STEP_ROWS]` | Whether or not to include time segments with a 0 step count during the whole day.                          |
+|`[COMPUTE]`                            | Set to `True` to extract `FITBIT_STEPS_INTRADAY` features from the `RAPIDS` provider|
+|`[FEATURES]`                           |         Features to be computed from steps intraday data, see table below           |
+|`[REFERENCE_HOUR]`                     | The reference point from which `firststeptime` or `laststeptime` is to be computed, default is midnight |
+|`[THRESHOLD_ACTIVE_BOUT]`              | Every minute with Fitbit steps data wil be labelled as `sedentary` if its step count is below this threshold, otherwise, `active`.    |  
+|`[THRESHOLD_DEVICE_NONWEAR_TIME]`      | Number of consecutive minutes with zero step counts at or above which to label as a period of device non-wear time. Used only to calculate `uncensoredmeancadence` feature. Default is 60. Set to 1 to calculate a measure of mean non-zero walking cadence or to 0 to disable.  |   
+|`[THRESHOLD_MINUTE_LEVEL_STEP_COUNT]`  | Rows with minute-level step counts above this threshold will be excluded from feature computation. Default is 1000; set to 0 to disable. |  
+|`[INCLUDE_ZERO_STEP_ROWS]`             | Whether or not to include time segments with a 0 step count during the whole day.                          |
 
 
 Features description for `[FITBIT_STEPS_INTRADAY][PROVIDERS][RAPIDS]`:
 
-|Feature                    |Units          |Description                                                  |
-|-------------------------- |-------------- |-------------------------------------------------------------|
-|sumsteps                   |steps          |The total step count during a time segment.
-|maxsteps                   |steps          |The maximum step count during a time segment.
-|minsteps                   |steps          |The minimum step count during a time segment.
-|avgsteps                   |steps          |The average step count during a time segment.
-|stdsteps                   |steps          |The standard deviation of step count during a time segment.
-|firststeptime              |minutes        |Minutes until the first non-zero step count.
-|laststeptime               |minutes        |Minutes until the last non-zero step count.
-|countepisodesedentarybout  |bouts          |Number of sedentary bouts during a time segment.
-|sumdurationsedentarybout   |minutes        |Total duration of all sedentary bouts during a time segment.
-|maxdurationsedentarybout   |minutes        |The maximum duration of any sedentary bout during a time segment.
-|mindurationsedentarybout   |minutes        |The minimum duration of any sedentary bout during a time segment.
-|avgdurationsedentarybout   |minutes        |The average duration of sedentary bouts during a time segment.
-|stddurationsedentarybout   |minutes        |The standard deviation of the duration of sedentary bouts during a time segment.
-|countepisodeactivebout     |bouts          |Number of active bouts during a time segment.
-|sumdurationactivebout      |minutes        |Total duration of all active bouts during a time segment.
-|maxdurationactivebout      |minutes        |The maximum duration of any active bout during a time segment.
-|mindurationactivebout      |minutes        |The minimum duration of any active bout during a time segment.
-|avgdurationactivebout      |minutes        |The average duration of active bouts during a time segment.
-|stddurationactivebout      |minutes        |The standard deviation of the duration of active bouts during a time segment.
+|Feature                                    |Units          |Description                                                  |
+|------------------------------------------ |-------------- |-------------------------------------------------------------|
+|sumsteps                                   |steps          |The total step count during a time segment.
+|maxsteps                                   |steps          |The maximum step count during a time segment.
+|minsteps                                   |steps          |The minimum step count during a time segment.
+|avgsteps                                   |steps          |The average step count during a time segment.
+|stdsteps                                   |steps          |The standard deviation of step count during a time segment.
+|firststeptime                              |minutes        |Minutes until the first non-zero step count.
+|laststeptime                               |minutes        |Minutes until the last non-zero step count.
+|countepisodesedentarybout                  |bouts          |Number of sedentary bouts during a time segment.
+|sumdurationsedentarybout                   |minutes        |Total duration of all sedentary bouts during a time segment.
+|maxdurationsedentarybout                   |minutes        |The maximum duration of any sedentary bout during a time segment.
+|mindurationsedentarybout                   |minutes        |The minimum duration of any sedentary bout during a time segment.
+|avgdurationsedentarybout                   |minutes        |The average duration of sedentary bouts during a time segment.
+|stddurationsedentarybout                   |minutes        |The standard deviation of the duration of sedentary bouts during a time segment.
+|countepisodeactivebout                     |bouts          |Number of active bouts during a time segment.
+|sumdurationactivebout                      |minutes        |Total duration of all active bouts during a time segment.
+|maxdurationactivebout                      |minutes        |The maximum duration of any active bout during a time segment.
+|mindurationactivebout                      |minutes        |The minimum duration of any active bout during a time segment.
+|avgdurationactivebout                      |minutes        |The average duration of active bouts during a time segment.
+|stddurationactivebout                      |minutes        |The standard deviation of the duration of active bouts during a time segment.  
+|activetosedentarytransitionprobability     |unitless       |Active-to-sedentary transition probability (ASTP). The reciprocal of the mean active bout length during a time segment. Bounded by 0 and 1.    
+|sumdurationactivitylessthan5minutes        |minutes        |Sum of duration of active bouts less than 5 minutes in length during a time segment.   
+|sumdurationactivity5to105minutes           |minutes        |Sum of duration of active bouts between 5 and 10 minutes in length, inclusive, during a time segment.  
+|sumdurationactivitygreaterthan10minutes    |minutes        |Sum of duration of active bouts more than 10 minutes in length during a time segment.  
+|ginicoefficient                            |unitless       |Measure of (absolute, not squared) variability of active bout durations normalized by the average active bout duration during a time segment. When the Gini coefficient is close to 1, it indicates that total time is accumulated via a small number of longer bouts. Conversely, when the Gini coefficient is close to 0, it indicates that all bouts contribute equally to total time.  
+|meancadence                                |steps/minute   |The average steps/minute during a time segment.  
+|uncensoredmeancadence                      |steps/minute   |The total raw steps accumulated divided by device wear time during a time segment.  
+|peak1minutecadence                         |steps/minute   |The steps/minute recorded for the highest single minute during a time segment; this is also equivalent to the maximum 1 minute cadence.  
+|peak30minutecadence                        |steps/minute   |The average steps/minute recorded for the 30 highest, but not necessarily consecutive, minutes during a time segment.  
+|peak60minutecadence                        |steps/minute   |The average steps/minute recorded for the 60 highest, but not necessarily consecutive, minutes during a time segment.  
+|max5minutecadence                          |steps/minute   |The average steps/minute of the maximum number of steps obtained over 5 continuous minutes during a time segment.  
+|max20minutecadence                         |steps/minute   |The average steps/minute of the maximum number of steps obtained over 20 continuous minutes during a time segment.  
+|max30minutecadence                         |steps/minute   |The average steps/minute of the maximum number of steps obtained over 30 continuous minutes during a time segment.  
+|max60minutecadence                         |steps/minute   |The average steps/minute of the maximum number of steps obtained over 60 continuous minutes during a time segment.  
+|totalminutes0cadence                       |minutes        |Total minutes with no movement (0 steps/minute) during a time segment; an indicator of non-ambulatory or sedentary time.  
+|totalminutes1to19cadence                   |minutes        |Total minutes with incidental movement ([1, 20) steps/minute) during a time segment.    
+|totalminutes20to39cadence                  |minutes        |Total minutes with sporadic movement ([20, 40) steps/minute) during a time segment.  
+|totalminutes40to59cadence                  |minutes        |Total minutes with purposeful steps ([40, 60) steps/minute) during a time segment.  
+|totalminutes60to79cadence                  |minutes        |Total minutes with slow walking ([60, 80) steps/minute) during a time segment.  
+|totalminutes80to99cadence                  |minutes        |Total minutes with medium walking ([80, 100) steps/minute) during a time segment.  
+|totalminutes100to119cadence                |minutes        |Total minutes with brisk walking ([100, 120) steps/minute) during a time segment.  
+|totalminutes120pluscadence                 |minutes        |Total minutes with all faster ambulation ([120, Inf) steps/minute) during a time segment.  
+|totalminutesabove0cadence                  |minutes        |Total minutes with any movement (>0 steps/minute) during a time segment.     
+|totalminutesabove19cadence                 |minutes        |Total minutes with non-incidental movement (>19 steps/minute) during a time segment.  
+|totalminutesabove100cadence                |minutes        |Total minutes with movement of at least moderate intensity (>100 steps/minute) during a time segment; this threshold was based on findings from ostensibly health individuals without movement impairments.    
 
 !!! note "Assumptions/Observations"
     
-    1. _Active and sedentary bouts_. If the step count per minute is smaller than `THRESHOLD_ACTIVE_BOUT` (default value is 10), that minute is labelled as sedentary, otherwise, is labelled as active. Active and sedentary bouts are periods of consecutive minutes labelled as `active` or `sedentary`.
+    1. _Active and sedentary bouts_. If the step count per minute is smaller than `THRESHOLD_ACTIVE_BOUT` (default value is 10), that minute is labelled as sedentary, otherwise, is labelled as active. Active and sedentary bouts are periods of consecutive minutes labelled as `active` or `sedentary`.  
+    2. Activity fragmentation features are based on the following papers: [Di et al., 2017](https://doi.org/10.1101/182337), [Schrack et al., 2019](https://doi.org/10.1093/gerona/gly243), and [Wanigatunga et al., 2019](https://doi.org/10.1001/jamanetworkopen.2019.12352)  
+    3. Walking cadence features are based on the following review paper: [Tudor-Locke et al., 2018](https://doi.org/10.1136/bjsports-2017-097628)  
 
--- src/features/fitbit_steps_intraday/rapids/main.py
+++ src/features/fitbit_steps_intraday/rapids/main.py
@@ -1,6 +1,46 @@
 import pandas as pd
 import numpy as np
 
+# calculate Gini coefficient, a measure of normalized variability. Adapted from: https://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+def calculateGiniCoefficient(x):
+    x = np.array(x) 
+    x = np.sort(x) 
+    n = len(x) 
+    i = np.arange(1, n+1) 
+    gini = (np.sum(((2*i)-n-1)*x))/(n*(np.sum(x)))
+    return gini
+
+# extract lower and upper bounds of cadence band from feature name
+def getCadenceBandBounds(feature):
+    band = feature.split("totalminutes")[1].split("cadence")[0]
+    if band == "0":
+        bounds = [0, 1]
+    elif band == "120plus":
+        bounds = [120, np.inf]
+    else:
+        bounds = [int(x) for x in band.split("to")]
+        bounds[1]+=1
+    return bounds
+
+# get device wear time based on definition from Tudor-Locke et al., 2017, which is used to compute uncensored average cadence feature
+def getDeviceWearTimes(steps_data, threshold_device_nonwear_time):
+    steps_data = steps_data.assign(iszerostepcount = np.where(steps_data["steps"] == 0, 1, 0), isnotzerostepcount = np.where(steps_data["steps"] != 0, 1, 0))
+ 
+    # put consecutive rows into the same group if they have the same values of "iszerostepcount", "local_timezone", and "local_segment"
+    steps_data["group_idx"] = (steps_data[["iszerostepcount", "local_timezone", "local_segment"]].shift() != steps_data[["iszerostepcount", "local_timezone", "local_segment"]]).any(axis = 1).cumsum()
+    weartimes = steps_data.groupby(["local_segment", "local_timezone", "group_idx"]).agg(sumsteps = ("steps", "sum"), zerostepduration = ("iszerostepcount", "sum"), nonzerostepduration = ("isnotzerostepcount", "sum"))
+
+    # create an indicator for groups of consecutive zero step counts with duration greater than or equal to the threshold (default: 60) if threshold was not set to 0
+    if threshold_device_nonwear_time == 0:
+        weartimes = weartimes.assign(isnonweartime = 0)
+    else:
+        weartimes = weartimes.assign(isnonweartime = np.where(weartimes["zerostepduration"] >= threshold_device_nonwear_time, 1, 0))
+    
+    # sum duration for rows where the isnonweartime indicator does not equal 1 to obtain device wear times
+    weartimes = weartimes.assign(weartimeduration = np.where(weartimes["isnonweartime"] != 1, weartimes["zerostepduration"] + weartimes["nonzerostepduration"], 0))
+
+    return weartimes
+
 def statsFeatures(steps_data, features_to_compute, features_type, steps_features, *args, **kwargs):
     if features_type == "steps" or features_type == "sumsteps":
         col_name = "steps"
@@ -31,6 +71,67 @@ def statsFeatures(steps_data, features_to_compute, features_type, steps_features
 
     return steps_features
 
+def activityFragmentationFeatures(steps_data, features_to_compute, steps_features, *args, **kwargs):
+    if ("activetosedentarytransitionprobability" in features_to_compute):
+        if not steps_data.empty:
+            steps_features["activetosedentarytransitionprobability"] = steps_data.groupby(["local_segment"])["duration"].agg(lambda x: 1/x.mean())
+        else:
+            steps_features["activetosedentarytransitionprobability"] = np.nan
+    if "sumdurationactivitylessthan5minutes" in features_to_compute:
+        if not steps_data.empty:
+            steps_features["sumdurationactivitylessthan5minutes"] = steps_data.groupby(["local_segment"]).apply(lambda x: x[x["duration"] < 5]["duration"].sum())
+        else:
+             steps_features["sumdurationactivitylessthan5minutes"] = np.nan
+    if "sumdurationactivity5to105minutes" in features_to_compute:
+        if not steps_data.empty:
+            steps_features["sumdurationactivity5to105minutes"] = steps_data.groupby(["local_segment"]).apply(lambda x: x[x["duration"].between(5, 10, inclusive = "both")]["duration"].sum())
+        else:
+            steps_features["sumdurationactivity5to105minutes"] = np.nan
+    if "sumdurationactivitygreaterthan10minutes" in features_to_compute:
+        if not steps_data.empty:
+            steps_features["sumdurationactivitygreaterthan10minutes"] = steps_data.groupby(["local_segment"]).apply(lambda x: x[x["duration"] > 10]["duration"].sum())
+        else:
+            steps_features["sumdurationactivitygreaterthan10minutes"] = np.nan
+    if "ginicoefficient" in features_to_compute:
+        if not steps_data.empty:
+            steps_features["ginicoefficient"] = steps_data.groupby(["local_segment"])["duration"].agg(lambda x: calculateGiniCoefficient(x))
+        else:
+            steps_features["ginicoefficient"] = np.nan
+            
+    return steps_features
+
+def walkingCadenceFeatures(steps_data, features_to_compute, steps_features, *args, **kwargs):
+    threshold_device_nonwear_time = kwargs["threshold_device_nonwear_time"]
+
+    peak_cadence_features = [x for x in features_to_compute if x.startswith("peak")]
+    max_cadence_features = [x for x in features_to_compute if x.startswith("max")]
+    cadence_band_features = [x for x in features_to_compute if (x.startswith("totalminutes") and not x.startswith("totalminutesabove"))]
+    cadence_threshold_features = [x for x in features_to_compute if x.startswith("totalminutesabove")]
+
+    if "meancadence" in features_to_compute:
+        steps_features["meancadence"] = steps_data.groupby(["local_segment"])["steps"].mean()
+    if "uncensoredmeancadence" in features_to_compute:
+        weartimes = getDeviceWearTimes(steps_data, threshold_device_nonwear_time)
+        steps_features["uncensoredmeancadence"] = weartimes.groupby(["local_segment"]).apply(lambda x: np.where(x["weartimeduration"].sum() > 0, x["sumsteps"].sum()/x["weartimeduration"].sum(), np.nan))
+    if peak_cadence_features:
+        for feature in peak_cadence_features:
+            n = int(feature.split("peak")[1].split("minutecadence")[0])
+            steps_features[feature] = steps_data.groupby(["local_segment"])["steps"].apply(lambda x: np.where(x.count() >= n, x.sort_values(ascending = False).head(n).mean(), np.nan))
+    if max_cadence_features:
+        for feature in max_cadence_features:
+            n = int(feature.split("max")[1].split("minutecadence")[0])
+            steps_features[feature] = steps_data.groupby(["local_segment"])["steps"].apply(lambda x: np.where(x.count() >= n, x.rolling(window = n, center = False).mean().max(), np.nan))
+    if cadence_band_features:
+        for feature in cadence_band_features:
+            bounds = getCadenceBandBounds(feature)
+            steps_features[feature] = steps_data.groupby(["local_segment"])["steps"].agg(lambda x: x.between(bounds[0], bounds[1], inclusive = "left").sum())
+    if cadence_threshold_features:
+        for feature in cadence_threshold_features:
+            n = int(feature.split("totalminutesabove")[1].split("cadence")[0])
+            steps_features[feature] = steps_data.groupby(["local_segment"])["steps"].agg(lambda x: (x > n).sum())
+
+    return steps_features
+
 def getBouts(steps_data):
 
     # put consecutive rows into the same group if they have the same values of "isactivebout", "local_timezone", and "local_segment"
@@ -43,7 +144,7 @@ def getBouts(steps_data):
 
     return bouts
 
-def extractStepsFeaturesFromIntradayData(steps_intraday_data, reference_hour, threshold_active_bout, intraday_features_to_compute_steps, intraday_features_to_compute_sedentarybout, intraday_features_to_compute_activebout, steps_intraday_features):
+def extractStepsFeaturesFromIntradayData(steps_intraday_data, reference_hour, threshold_active_bout, threshold_device_nonwear_time, intraday_features_to_compute_steps, intraday_features_to_compute_sedentarybout, intraday_features_to_compute_activebout, intraday_features_to_compute_activityfragmentation, intraday_features_to_compute_walkingcadence, steps_intraday_features):
     steps_intraday_features = pd.DataFrame()
 
     # statistics features of steps count
@@ -63,6 +164,12 @@ def extractStepsFeaturesFromIntradayData(steps_intraday_data, reference_hour, th
     active_bout = bouts[bouts["isactivebout"] == 1]
     steps_intraday_features = statsFeatures(active_bout, intraday_features_to_compute_activebout, "durationactivebout", steps_intraday_features)
 
+    # activity fragmentation features
+    steps_intraday_features = activityFragmentationFeatures(active_bout, intraday_features_to_compute_activityfragmentation, steps_intraday_features)
+
+    # walking cadence features
+    steps_intraday_features = walkingCadenceFeatures(steps_intraday_data, intraday_features_to_compute_walkingcadence, steps_intraday_features, threshold_device_nonwear_time=threshold_device_nonwear_time)
+
     steps_intraday_features.reset_index(inplace=True)
 
     return steps_intraday_features
@@ -73,6 +180,8 @@ def rapids_features(sensor_data_files, time_segment, provider, filter_data_by_se
 
     reference_hour = provider["REFERENCE_HOUR"]
     threshold_active_bout = provider["THRESHOLD_ACTIVE_BOUT"]
+    threshold_device_nonwear_time = provider["THRESHOLD_DEVICE_NONWEAR_TIME"]
+    threshold_minute_level_step_count = provider["THRESHOLD_MINUTE_LEVEL_STEP_COUNT"]
     include_zero_step_rows = provider["INCLUDE_ZERO_STEP_ROWS"]
 
     steps_intraday_data = pd.read_csv(sensor_data_files["sensor_data"])
@@ -82,14 +191,24 @@ def rapids_features(sensor_data_files, time_segment, provider, filter_data_by_se
     requested_intraday_features_steps = [x + "steps" if x not in ["firststeptime", "laststeptime"] else x for x in requested_intraday_features["STEPS"]]
     requested_intraday_features_sedentarybout = [x + "sedentarybout" for x in requested_intraday_features["SEDENTARY_BOUT"]]
     requested_intraday_features_activebout = [x + "activebout" for x in requested_intraday_features["ACTIVE_BOUT"]]
+    requested_intraday_features_activityfragmentation = requested_intraday_features["ACTIVITY_FRAGMENTATION"]
+    requested_intraday_features_walkingcadence = [x + "cadence" for x in requested_intraday_features["WALKING_CADENCE"]]
+
     # name of the features this function can compute
     base_intraday_features_steps = ["sumsteps", "maxsteps", "minsteps", "avgsteps", "stdsteps", "firststeptime", "laststeptime"]
     base_intraday_features_sedentarybout = ["countepisodesedentarybout", "sumdurationsedentarybout", "maxdurationsedentarybout", "mindurationsedentarybout", "avgdurationsedentarybout", "stddurationsedentarybout"]
     base_intraday_features_activebout = ["countepisodeactivebout", "sumdurationactivebout", "maxdurationactivebout", "mindurationactivebout", "avgdurationactivebout", "stddurationactivebout"]
+    base_intraday_features_activityfragmentation = ["activetosedentarytransitionprobability", "sumdurationactivitylessthan5minutes", "sumdurationactivity5to105minutes", "sumdurationactivitygreaterthan10minutes", "ginicoefficient"]
+    base_intraday_features_walkingcadence = ["meancadence", "uncensoredmeancadence", "peak1minutecadence", "peak30minutecadence", "peak60minutecadence", "max5minutecadence", "max20minutecadence", "max30minutecadence", "max60minutecadence",
+        "totalminutes0cadence", "totalminutes1to19cadence", "totalminutes20to39cadence", "totalminutes40to59cadence", "totalminutes60to79cadence", "totalminutes80to99cadence", "totalminutes100to119cadence", "totalminutes120pluscadence", 
+        "totalminutesabove0cadence", "totalminutesabove19cadence", "totalminutesabove100cadence"]
+
     # the subset of requested features this function can compute
     intraday_features_to_compute_steps = list(set(requested_intraday_features_steps) & set(base_intraday_features_steps))
     intraday_features_to_compute_sedentarybout = list(set(requested_intraday_features_sedentarybout) & set(base_intraday_features_sedentarybout))
     intraday_features_to_compute_activebout = list(set(requested_intraday_features_activebout) & set(base_intraday_features_activebout))
+    intraday_features_to_compute_activityfragmentation  = list(set(requested_intraday_features_activityfragmentation) & set(base_intraday_features_activityfragmentation))
+    intraday_features_to_compute_walkingcadence  = list(set(requested_intraday_features_walkingcadence) & set(base_intraday_features_walkingcadence))
 
     intraday_features_to_compute = intraday_features_to_compute_steps + intraday_features_to_compute_sedentarybout + intraday_features_to_compute_activebout
 
@@ -99,12 +218,17 @@ def rapids_features(sensor_data_files, time_segment, provider, filter_data_by_se
         zerocountdates = dailycountstep[dailycountstep["steps"] == 0].index.tolist()
         steps_intraday_data = steps_intraday_data[~steps_intraday_data["local_date"].isin(zerocountdates)]
 
+    # exclude rows with minute-level step counts above the specified threshold (e.g., 1000 steps/minute)
+    if (not steps_intraday_data.empty) and (threshold_minute_level_step_count > 0):
+        highstepcountrows = steps_intraday_data[steps_intraday_data["steps"] > threshold_minute_level_step_count].index.tolist()
+        steps_intraday_data = steps_intraday_data[~steps_intraday_data.index.isin(highstepcountrows)]
+
     # extract features from intraday features
     steps_intraday_features = pd.DataFrame(columns=["local_segment"] + intraday_features_to_compute)
     if not steps_intraday_data.empty:
         steps_intraday_data = filter_data_by_segment(steps_intraday_data, time_segment)
 
         if not steps_intraday_data.empty:
-            steps_intraday_features = extractStepsFeaturesFromIntradayData(steps_intraday_data, reference_hour, threshold_active_bout, intraday_features_to_compute_steps, intraday_features_to_compute_sedentarybout, intraday_features_to_compute_activebout, steps_intraday_features)
+            steps_intraday_features = extractStepsFeaturesFromIntradayData(steps_intraday_data, reference_hour, threshold_active_bout, threshold_device_nonwear_time, intraday_features_to_compute_steps, intraday_features_to_compute_sedentarybout, intraday_features_to_compute_activebout, intraday_features_to_compute_activityfragmentation, intraday_features_to_compute_walkingcadence, steps_intraday_features)
     
     return steps_intraday_features
