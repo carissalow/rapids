6d6a67650edde97c17c995d8e901af309a389766
amal3ndu <amalendubokil@gmail.com>
2024-01-21 00:21:33 -0500

    Deployed 67e0caa to 0.2 with MkDocs 1.5.3 and mike 2.0.0

--- 0.2/javascripts/extra.js
+++ 0.2/javascripts/extra.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", window.location + "../versions.json");
+    xhr.open("GET", window.location.origin + "/versions.json");
     xhr.onload = function() {
       var versions = JSON.parse(this.responseText);
       latest_version = ""
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", function() {
           if(versions[id]["aliases"].length > 0 && versions[id]["aliases"].includes("latest")) 
               latest_version = "/" + versions[id].version + "/"
       if(!window.location.pathname.includes("/latest/") && (latest_version.length > 0 && !window.location.pathname.includes(latest_version)))
-          document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are seeing the docs for a previous version of RAPIDS, <a href='" + window.location.href + "latest/'>click here to go to the latest</a></div>"
+          document.querySelector("div[data-md-component=announce]").innerHTML = "<div id='announce-msg'>You are seeing the docs for a previous version of RAPIDS, <a href='http://www.rapids.science/'>click here to go to the latest</a></div>"
     };
     xhr.send();
   });
