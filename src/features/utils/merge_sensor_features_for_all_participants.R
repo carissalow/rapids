996cb55d01bdd0705db25adab8583228e6789d10
jenniferfedor <fedorjm@gmail.com>
2025-11-07 21:24:18 +0000

    Relocate participant ID column in merged features

--- src/features/utils/merge_sensor_features_for_all_participants.R
+++ src/features/utils/merge_sensor_features_for_all_participants.R
@@ -7,11 +7,13 @@ library(stringr)
 
 feature_files  <- snakemake@input[["feature_files"]]
 
-
-features_of_all_participants <- tibble(filename = feature_files) %>% # create a data frame
-  mutate(file_contents = map(filename, ~ read.csv(., stringsAsFactors = F, colClasses = c(local_segment = "character", local_segment_label = "character", local_segment_start_datetime="character", local_segment_end_datetime="character"))),
-         pid = str_match(filename, ".*/(.*)/all_sensor_features.csv")[,2]) %>%
+features_of_all_participants <- tibble(filename = feature_files) %>% 
+  mutate(
+    file_contents = map(filename, ~ read.csv(., stringsAsFactors = F, colClasses = c(local_segment = "character", local_segment_label = "character", local_segment_start_datetime="character", local_segment_end_datetime="character"))),
+    pid = str_match(filename, ".*/(.*)/all_sensor_features.csv")[,2]
+  ) %>%
   unnest(cols = c(file_contents)) %>%
-  select(-filename)
+  select(-filename) %>%
+  select(pid, everything())
 
 write.csv(features_of_all_participants, snakemake@output[[1]], row.names = FALSE)
